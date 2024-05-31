8c71505b906d969d3b27fec88d1f624e4110dc20
Conrado Garcia Berrotaran <conradogarciab@gmail.com>
2024-05-31 12:21:35 -0300

    Ignores config files

--- .config/nvim/lua/cono/plugins/nvim-tree.lua
+++ .config/nvim/lua/cono/plugins/nvim-tree.lua
@@ -27,7 +27,7 @@ return {
 				},
 			},
 			filters = {
-				custom = { ".DS_Store" },
+				custom = { ".DS_Store", ".git", "*.pytest_cache", "__pycache__", ".python-version", "log.txt" },
 			},
 			git = {
 				ignore = false,
