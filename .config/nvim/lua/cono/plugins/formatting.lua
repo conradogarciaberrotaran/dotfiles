c2037079a73f7beabe7cb7e79165fa97352f48b8
Conrado Garcia Berrotaran <conradogarciab@gmail.com>
2024-06-05 09:13:50 -0300

    Fix all on save for Ruff LSP

--- .config/nvim/lua/cono/plugins/formatting.lua
+++ .config/nvim/lua/cono/plugins/formatting.lua
@@ -26,5 +26,16 @@ return {
 				timeout_ms = 1000,
 			},
 		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				if vim.bo.ft == "python" then
+					vim.lsp.buf.code_action({
+						context = { only = { "source.fixAll.ruff" } },
+						apply = true,
+					})
+				end
+			end,
+		})
 	end,
 }
